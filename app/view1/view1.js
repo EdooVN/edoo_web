d38fe318f5393185af23b98ed56e55f2232bb624
Tu Tv <tutv95@gmail.com>
2016-07-30 15:06:06 +0700

    Add bootstrap

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,30 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'ui.bootstrap'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.controller('View1Ctrl', [function() {
+    .controller('View1Ctrl', ['$scope', function ($scope) {
 
-}]);
+    }])
+
+    .controller('mainController', ['$scope', function ($scope) {
+
+        // BUTTONS ======================
+
+        // define some random object
+        $scope.bigData = {};
+
+        $scope.bigData.breakfast = false;
+        $scope.bigData.lunch = false;
+        $scope.bigData.dinner = false;
+
+        // COLLAPSE =====================
+        $scope.isCollapsed = false;
+
+    }]);
