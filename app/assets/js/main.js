685d323fd433925ec3c371c7ad55720145097b00
daitd <dai@foobla.com>
2016-08-23 00:14:32 +0700

    update nprogress bar

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,5 +1,12 @@
 (function ($) {
     $(document).ready(function () {
 
+        $(window).on('http_start', function () {
+            NProgress.start();
+            console.log('start');
+        }).on('http_complete', function () {
+            NProgress.done();
+        });
+
     });
 })(jQuery);
