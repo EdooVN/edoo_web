376e3c021753f50ec42d18022fe9d8c3bd2ba8bb
Tu TV <tutv95@gmail.com>
2016-09-02 10:23:54 +0700

    Updated page title welcome

--- app/modules/pages/welcome.ctrl.js
+++ app/modules/pages/welcome.ctrl.js
@@ -2,8 +2,10 @@
     'use strict';
 
     angular.module('app.core')
-        .controller('WelcomeController', function ($scope, localStorageService, $location) {
+        .controller('WelcomeController', function ($scope, localStorageService, $location, PageValues) {
             var token = localStorageService.get('user_token');
+
+            PageValues.title = 'Chào mừng bạn đến với mạng xã hội Edoo';
 
             if (token) {
                 $location.path('/class');
