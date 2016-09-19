f0ef0e4bc37dbaeb8d508c7281680293e0240e11
Max <tutv95@gmail.com>
2016-09-19 22:55:29 +0700

    Fixes missing NotificationService

--- app/modules/classes/index.ctrl.js
+++ app/modules/classes/index.ctrl.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('app.core')
-        .controller('ClassIndexController', function (StorageService, ClassService, PageValues) {
+        .controller('ClassIndexController', function (StorageService, ClassService, PageValues, NotificationService) {
             this.listClass = [];
             var mv = this;
 
