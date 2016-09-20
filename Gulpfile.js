a4f632255a7b4517c3ed65f1c0859df8c755d5c7
Max <tutv95@gmail.com>
2016-09-20 11:21:17 +0700

    Test gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,13 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     live_reload = require('gulp-livereload'),
     sass = require('gulp-sass'),
-    source_maps = require('gulp-sourcemaps');
+    source_maps = require('gulp-sourcemaps'),
+    useref = require('gulp-useref'),
+    gulpif = require('gulp-if'),
+    jsmin = require('gulp-jsmin'),
+    minifyCss = require('gulp-clean-css'),
+    usemin = require('gulp-usemin');
+
 
 // Default Task
 gulp.task('default', ['watch']);
@@ -30,4 +36,42 @@ gulp.task('sass', function () {
         .pipe(source_maps.write())
         .pipe(gulp.dest('app/assets/css'))
         .pipe(live_reload());
+});
+
+gulp.task('concat', function () {
+    return gulp.src('app/index.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', jsmin()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('vendor', function () {
+    var original_dir = 'app/';
+
+    var vendors = [
+        'bower_components/jquery/dist/jquery.js',
+        'bower_components/bootstrap/dist/js/bootstrap.js',
+        'bower_components/tinymce-dist/tinymce.jquery.js',
+        'bower_components/moment/moment.js',
+        'bower_components/moment/locale/vi.js',
+        'bower_components/noty/js/noty/packaged/jquery.noty.packaged.js',
+        'assets/js/theme.noty.js',
+        'bower_components/angular/angular.js',
+        'bower_components/angular-sanitize/angular-sanitize.js',
+        'bower_components/angular-ui-router/release/angular-ui-router.js',
+        'bower_components/angular-local-storage/dist/angular-local-storage.js',
+        'bower_components/angular-loading-bar/src/loading-bar.js',
+        'bower_components/angular-ui-tinymce/src/tinymce.js',
+        'bower_components/angular-moment/angular-moment.js',
+        'bower_components/angular-animate/angular-animate.js',
+        'assets/js/main.js'
+    ];
+
+    for (var i = 0; i < vendors.length; i++) {
+        vendors[i] = original_dir + vendors[i];
+    }
+
+    return gulp.src(vendors)
+        .pipe(gulp.dest('dist/vendor'));
 });
