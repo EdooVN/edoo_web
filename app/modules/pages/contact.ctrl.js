7a7b96b7dca90bc29d1e3b867a58a9576294a0c6
thaibm <thaibm.uet@gmail.com>
2016-09-23 22:55:43 +0700

    Contace page. Some bugs in submit button.

--- app/modules/pages/contact.ctrl.js
+++ app/modules/pages/contact.ctrl.js
@@ -4,5 +4,14 @@
     angular.module('app.core')
         .controller('ContactPageController', function (PageValues) {
             PageValues.title = 'Liên hệ';
+
+
         });
+
+    /*angular.module('ContactForm').controller('ContactCtrl', function ($scope) {
+        $scope.submitContactForm = function () {
+            console.log('submit nef! :v');
+        }
+    });*/
+
 })();
