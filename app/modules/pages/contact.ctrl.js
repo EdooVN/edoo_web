2bc3aa381cd3a52718cb71793b5eba57662311d3
thaibm <thaibm.uet@gmail.com>
2016-09-24 11:15:54 +0700

    add under-heading.png

--- app/modules/pages/contact.ctrl.js
+++ app/modules/pages/contact.ctrl.js
@@ -4,14 +4,10 @@
     angular.module('app.core')
         .controller('ContactPageController', function (PageValues) {
             PageValues.title = 'Liên hệ';
-
-
         });
-
-    /*angular.module('ContactForm').controller('ContactCtrl', function ($scope) {
+    angular.module("core",[]).controller("ContactCtrl", function($scope) {
         $scope.submitContactForm = function () {
             console.log('submit nef! :v');
         }
-    });*/
-
+    });
 })();
