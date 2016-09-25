a47fe844e47b65986916c58a0c9a3e4c3082c00c
thaibm <thaibm.uet@gmail.com>
2016-09-25 21:26:37 +0700

    Update contact form. New design.

--- app/modules/pages/contact.ctrl.js
+++ app/modules/pages/contact.ctrl.js
@@ -4,12 +4,10 @@
     angular.module('app.core')
         .controller('ContactPageController', function (PageValues) {
             PageValues.title = 'Liên hệ';
-
             var mv = this;
             mv.submitContactForm = submitContactForm;
-
             function submitContactForm() {
-                console.log('submit nef! :v');
+
             }
         });
 })();
