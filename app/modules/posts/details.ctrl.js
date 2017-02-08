c5c39bfe7940767b2eb4d051ae54bc90511a80de
Max <tutv95@gmail.com>
2017-02-08 23:04:24 +0700

    Create post

--- app/modules/posts/details.ctrl.js
+++ app/modules/posts/details.ctrl.js
@@ -30,12 +30,9 @@
             $scope.f = {};
 
             mv.tinymceOptions = {
-                plugins: [
-                    'advlist autolink lists link image charmap print preview anchor',
-                    'searchreplace visualblocks code fullscreen',
-                    'insertdatetime media table contextmenu paste code'
-                ],
-                toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+                plugins: ['link', 'autoresize', 'fullscreen', 'preview'],
+                menubar: false,
+                toolbar: 'bold italic | bullist numlist | blockquote | link',
                 content_css: '//www.tinymce.com/css/codepen.min.css'
             };
 
