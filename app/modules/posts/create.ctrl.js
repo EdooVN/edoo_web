051427663077c91b033eab161f74a5cfab4bbd1f
Max <tutv95@gmail.com>
2017-02-08 23:01:46 +0700

    Create post

--- app/modules/posts/create.ctrl.js
+++ app/modules/posts/create.ctrl.js
@@ -22,12 +22,9 @@
             mv.newPost.is_incognito = "0";
 
             mv.tinymceOptions = {
-                plugins: [
-                    'advlist autolink lists link image charmap print preview anchor',
-                    'searchreplace visualblocks code fullscreen',
-                    'insertdatetime media table contextmenu paste code'
-                ],
-                toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+                plugins: ['link', 'autoresize', 'fullscreen', 'preview', 'image'],
+                menubar: false,
+                toolbar: 'bold italic | bullist numlist | blockquote | link | image',
                 content_css: '//www.tinymce.com/css/codepen.min.css'
             };
 
